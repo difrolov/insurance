f2c5ea1ea194bdaf019e6ac8a658b65d4791ad64
difrolov <d.i.frolov@yandex.ru>
2012-08-13 21:46:44 +0400

    test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+//вот тут-то все и началось
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
