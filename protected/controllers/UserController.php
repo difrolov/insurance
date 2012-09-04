52b94749f1c4c08ee0213ee2c768feec10208918
difrolov <frol1738@rambler.ru>
2012-09-05 00:39:17 +0400

    autoriz

---
+++ protected/controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+class UserController extends Controller
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+*/
+	public function actionLogin()
+	{
+		$model=new Users;
+		$this->performAjaxValidation($model);
+		if (isset($_POST['Users'])){
+			// Аутентифицируем пользователя по имени и паролю
+			$identity=new UserIdentity($_POST['Users']['login'],$_POST['Users']['password']);
+			if($identity->authenticate()){
+				Yii::app()->user->login($identity);
+				$this->redirect('index');
+			}else{
+				echo $identity->errorMessage;
+			}
+			// Выходим
+			Yii::app()->user->logout();
+		}
+		$this->render('login',array('model'=>$model));
+	}
+
+	protected function performAjaxValidation($model)
+	{
+		if(isset($_POST['ajax']) && $_POST['ajax']==='user-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+	}
+
+}
---
+++ protected/views/user/login.php
@@ -0,0 +1,34 @@
+<div class="form">
+<?php $form=$this->beginWidget('CActiveForm', array(
+    'id'=>'user-form',
+    'enableAjaxValidation'=>true,
+    'enableClientValidation'=>false,
+	'focus'=>array($model,'login'),
+	'clientOptions'=>array('validateOnSubmit'=>true),
+)); ?>
+
+<?php echo $form->errorSummary($model); ?>
+
+<div class="row">
+<?php echo $form->label($model,'login'); ?>
+<?php echo $form->textField($model,'login') ?>
+<?php echo $form->error($model,'login'); ?>
+</div>
+
+<div class="row">
+<?php echo $form->label($model,'password'); ?>
+<?php echo $form->passwordField($model,'password') ?>
+<?php echo $form->error($model,'password'); ?>
+</div>
+
+<div class="row rememberMe">
+<?php echo $form->checkBox($model,'rememberMe'); ?>
+<?php echo $form->label($model,'rememberMe'); ?>
+</div>
+
+<div class="row submit">
+<?php echo CHtml::submitButton('Войти'); ?>
+</div>
+
+<?php $this->endWidget(); ?>
+</div><!-- form -->
