d99fcd6d2db1764c451100055e08a0e73ab50756
serge-serg <srgg67@gmail.com>
2012-09-23 16:09:35 +0400

    Настройка обработки алиасов (скрыто).

--- protected/views/malomu_i_srednemu_biznesu/index.php
+++ protected/views/malomu_i_srednemu_biznesu/index.php
@@ -2,6 +2,7 @@
 /* @var $this SiteController */
 /* @var $model ContactForm */
 /* @var $form CActiveForm */
+var_dump("<h1>this:</h1><pre>",$this,"</pre>");
 setHTML::setPageContent( 
 				$this,
 				'Малому и среднему бизнесу',
--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -7,7 +7,6 @@ $this->breadcrumbs=array(
 	'Error',
 );
 ?>
-
 <h2>Error <?php echo $code; ?></h2>
 
 <div class="error">
