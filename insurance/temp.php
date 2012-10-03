b1431f85c37376fdf683f5dd28a5a74889906e0f
serge-serg <srgg67@gmail.com>
2012-10-03 10:33:02 +0400

    Дальнейшая разработка скрипта генерации прототипа макета (тестовая версия).

--- insurance/temp.php
+++ insurance/temp.php
@@ -69,13 +69,48 @@ arrSchema=new Array(  '100',  //0
 					  '40i', //13
 					  '40s'	 //14
 					);
-function handleTmpl(index){
-  try{	
-  	var tmplSchema=arrSchema[index];
-  	var colsCount=parseInt(tmplSchema.substring(0,1));
-	if (tmplSchema.substring(1,2)!='0') colsCount+=1;
-	if (tmplSchema.substring(2)!='0') colsCount+=1;
-	alert(tmplSchema+': '+colsCount+'\n[1]= '+tmplSchema.substring(1,2)+', [2]= '+tmplSchema.substring(2));
+function handleTmpl(tmplSchema){
+  try{ //alert(tmplSchema);	
+  	//var tmplSchema=arrSchema[tSchema];
+  	// получить количество блоков:
+	var colsCountInit=colsCount=parseInt(tmplSchema.substring(0,1));
+	var tmplValue2=tmplSchema.substring(1,2);
+	var tmplValue3=tmplSchema.substring(2);
+	if (tmplValue2!='0') colsCount+=1;
+	if (tmplValue3!='0') colsCount+=1;
+	var tmplBlock='<div class="first">';
+	for (i=0;i<colsCount;i++){
+		if (i==0) { // назначить ширину первой колонке:
+			switch(colsCountInit){ // первое значение схемы - колич. колонок 
+				case 1:
+					firstColWidth='200';
+						break;
+				case 2:
+					firstColWidth='99';
+						break;
+				case 3:
+					firstColWidth='66';
+						break;
+				case 4:
+					firstColWidth='49';
+						break;
+			}
+			tmplBlock+='<div class="borderRight fLeft hFull" style="width:'+firstColWidth+'px;">left</div>';	
+		}else{
+			if (i==1){
+			}else if (i==2){
+			}
+			tmplBlock+='<div class="';
+			tmplBlock+='fLeft hColMiddle';
+			tmplBlock+='" style="';
+			tmplBlock+='width:58';
+			tmplBlock+='px;">Content comes here!</div>';
+		}
+	}
+	tmplBlock+="</div>";
+	//alert(tmplBlock);
+	document.getElementById('tmplPlace').innerHTML=tmplBlock;
+	//alert(tmplSchema+': '+colsCount+'\n[1]= '+tmplSchema.substring(1,2)+', [2]= '+tmplSchema.substring(2));
   }catch(e){
 	  alert(e.message);
   }
@@ -85,7 +120,7 @@ function handleTmpl(index){
 <body>
 <h4>30s</h4>
 <div class="first">
-    <div class="borderRight fLeft hFull" style="width:58px;">left</div>
+    <div class="borderRight fLeft hFull" style="width:'+firstColWidth+'px;">left</div>
     <div class="fLeft hColMiddle" style="width:58px;">inside</div>
     <div class="borderLeft fLeft hColMiddle" style="width:58px;">right</div>
     <div class="borderTop fRight hFooter" style="width:125px;">footer</div>
@@ -99,7 +134,8 @@ function handleTmpl(index){
     <div class="borderTop fRight hFooter" style="width:125px;">footer</div>
 </div>
 <hr>
-<input type="text" id="int" size="1">
+<input type="text" id="int" size="1" value="30s">
 <a href="javascript:handleTmpl(document.getElementById('int').value);">check tmplSchema </a>
+<div id="tmplPlace"></div>
 </body>
 </html>
