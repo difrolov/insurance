943d748b26218b19f54edba82f0a3a62f6cc2257
serge-serg <srgg67@gmail.com>
2012-10-06 00:27:50 +0400

    Добавление процедуры удаления модулей.

--- insurance/js/admin/generator/manage_template.php
+++ insurance/js/admin/generator/manage_template.php
@@ -9,20 +9,51 @@ function addModuleIntoBlock(event,divBlock){
 	   ) {
 		var newModule=document.createElement(srcEl.tagName);
 		tBlock.appendChild(newModule);
-		newModule.innerHTML=srcEl.innerHTML;
+		var arrows=document.createElement('div');
+		var content=document.createElement('div');
+		var remove=document.createElement('div');
+		
+		newModule.appendChild(arrows);
+		newModule.appendChild(content);
+		newModule.appendChild(remove);
+		
+		arrows.innerHTML='*<br>*';
+		content.innerHTML=srcEl.innerHTML;
+		remove.innerHTML='<a href="#" onClick="removeModule(this);return false;">del</a>';
+		
+		$(arrows).css({
+			display:'inline-block'
+		});
+		$(content).css({
+			position:'absolute',
+			left:'20px',
+			right:'20px',
+			top:'8px'
+		});
+		$(remove).css({
+			position:'absolute',
+			right:'4px',
+			top:'0'
+		});
+		
 		var sBg=$(srcEl).css('background-color');
 		$(newModule).css({
 			backgroundColor:sBg,
 			borderRadius:'8px',
 			color:'#FFF',
 			margin: '0 10px 4px 10px',
-			padding: '20px'
+			padding: '10px',
+			position: 'relative'
 		});
 	}
   }catch(e){
 	  alert(e.message);
   }	
 }
+function removeModule(objSrc){
+	$(objSrc.parentNode.parentNode).remove();
+}
+//
 function selectColumn(event,divBlock){ 
   try{
 	var srcEl=(event.target)? event.target:event.srcElement;
