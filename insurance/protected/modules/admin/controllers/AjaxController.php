6128b802b9ce55cfe8eda48f062df3619c1d0796
serge-serg <srgg67@gmail.com>
2012-10-27 00:15:52 +0400

    Убран текст-заглушка.

--- insurance/protected/modules/admin/controllers/AjaxController.php
+++ insurance/protected/modules/admin/controllers/AjaxController.php
@@ -16,55 +16,5 @@ class AjaxController extends Controller
 			$model = InsurArticleContent::model()->find(array('condition'=>"id=".$article_id));
 		}
 		return $model->content;
-		/* $sql='
-			SELECT ....';
-			//return Yii::app()->db->createCommand($sql)->queryAll();
-		return "<p>
-		Это - тестовый абзац № 1:
-		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-
-		diam nonumy eirmod tempor invidunt ut labore et dolore magna
-
-		aliquyam erat, sed diam voluptua. At vero eos et accusam et
-
-		justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-
-		sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-
-		<p>
-		А это, это уже № 2:
-		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-
-		diam nonumy eirmod tempor invidunt ut labore et dolore magna
-
-		aliquyam erat, sed diam voluptua. At vero eos et accusam et
-
-		justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-
-		sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-
-		<p>
-		И последнее, но не в последнюю очередь -  № 3!:
-		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-
-		diam nonumy eirmod tempor invidunt ut labore et dolore magna
-
-		aliquyam erat, sed diam voluptua. At vero eos et accusam et
-
-		justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-
-		sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-		<p>
-		А вот нифига-то и не в последнюю! Натебе № 4!:
-		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-
-		diam nonumy eirmod tempor invidunt ut labore et dolore magna
-
-		aliquyam erat, sed diam voluptua. At vero eos et accusam et
-
-		justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-
-		sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-		"; */
 	}
 }
