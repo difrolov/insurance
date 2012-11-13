f5ccac4fdf2290354ceb0ac3002c41562fbd2515
serge-serg <srgg67@gmail.com>
2012-11-13 17:30:07 +0400

    Добработка стилей макета.

--- insurance/protected/views/layouts/default.php
+++ insurance/protected/views/layouts/default.php
@@ -2,7 +2,7 @@
 <body>
 <div align="center">
   <div align="left" class="container" id="page">
-    <div id="fit_height">
+    <div id="fit_height"<? if(isset($_GET['test_bg'])){?> style="background:url(<?=Yii::app()->request->getBaseUrl(true)?>/_docs/sources/BODY.gif) -20px 0 no-repeat;"<? }?>>
 	  <div id="header">
 	<?	// /components/helpers
 		setHTML::buildLogosBlock();
@@ -17,13 +17,15 @@
 		setHTML::buildDropDownMenu();	// выпадающее меню	
 															?>
 	  </div>
+    <?	/**/?> 
 		<div id="main_submenu" align="right" style="position:relative;">
 	<?	setHTML::buildMainMenu($this,-2);
 	
 		setHTML::buildDropDownMenu(-2);
 												?>
 		</div>
-	<?	setHTML::buildBreadCrumbs();
+	<?	
+		setHTML::buildBreadCrumbs();
 		
 		echo $content;?>
   	</div>
