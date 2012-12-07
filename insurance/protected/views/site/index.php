fd2742f8d033a8600bae10430fac1fe1d898beae
serge-serg <srgg67@gmail.com>
2012-12-07 12:00:45 +0400

    Баннер(ный блок) №2 вынесен в отдельный файл components/submodules/banners2.php.

--- insurance/protected/views/site/index.php
+++ insurance/protected/views/site/index.php
@@ -177,11 +177,8 @@ $( function(){
     <li><p>Круглосуточная поддержка клиентов.</p></li>
   </ol>
 </div>
-<div id="content_from_right"><?
-$arrBan2=setHTML::getBannersAsObjects('inside');
-$bsrc=(isset($arrBan2[0]['src']))? $arrBan2[0]['src']:'';
-$blink=(isset($arrBan2[0]['link']))? $arrBan2[0]['link']:'';
-?><a href="<?=$baseURL?><? Data::buildAliasPath($blink);?>"><img src="<?=$baseURL.$bsrc?>" id="company_museum"></a>
+<div id="content_from_right"><?	
+require_once Yii::getPathOfAlias('webroot').'/protected/components/submodules/banners2.php';?>	
 </div>
 <?	require_once Yii::getPathOfAlias('webroot').'/protected/components/modules/species/default.php';
 	if ($tp){?><h3>/tblSlides</h3><? }?>
